791638431052208246fb9fa3c90132e03bebe689
youfeng <youfeng1024@gmail.com>
2024-12-10 06:52:10 +0800

    modified:   openi-train.py

--- openi-train.py
+++ openi-train.py
@@ -4,30 +4,28 @@ import shutil
 
 import os
 
-os.system("git clone https://github.com/youfeng1024/MASTER.git && cd MASTER")
+# Clone the repository
+os.system("git clone https://github.com/youfeng1024/MASTER.git")
+
+# Change working directory to the cloned repository
+os.chdir("MASTER")
+
+# Download the dataset
 os.system("curl -u youfeng1024@gmail.com:ycziyobue6ir3l2e -O https://app.koofr.net/dav/OneDrive/dataset/opensource.zip")
+
+# Unzip the dataset
 os.system("unzip opensource.zip -d data/opensource/")
+
+# Run the main script and save output
 os.system("python main.py | tee output.txt")
-
-
-# 移动到输出目录
-def copy_current_directory_to_output_path(output_path):
-    # 获取当前工作目录
-    current_directory = os.getcwd()
-
-    # 构建目标目录路径
-    target_directory = os.path.join(output_path, os.path.basename(current_directory))
-
-    # 确保目标路径存在，如果不存在则创建
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
-
-    # 复制整个目录树
-    shutil.copytree(current_directory, target_directory, dirs_exist_ok=True)
-    print(f"Copied {current_directory} to {target_directory}")
 
 c2net_context = prepare()
 output_path = c2net_context.output_path
-copy_current_directory_to_output_path(output_path)
+# 获取当前工作目录
+current_directory = os.getcwd()
+
+# 复制整个目录树
+shutil.copytree(current_directory, output_path, dirs_exist_ok=True)
+print(f"Copied {current_directory} to {output_path}")
 
 upload_output()
