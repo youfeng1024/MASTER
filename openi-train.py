0fd68b53ee80402ba90dd20adb263c28bb8dc5dc
youfeng <youfeng1024@gmail.com>
2024-12-11 00:41:23 +0800

    modified:   openi-train.py

--- openi-train.py
+++ openi-train.py
@@ -9,7 +9,7 @@ commands = """
 git clone https://github.com/youfeng1024/MASTER.git
 cd MASTER
 wget -O opensource.zip -c https://dlink.host/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvYy8wMWI3Nzg3OWYwZDUxNzZlL0VWRzhFQWZfNVpoRnZmclZjT3ZWSUxJQlZhbmR0VjNJelJseTdobWhPckxTQUE_ZT0yVFNlZmI.zip
-unzip opensource.zip -d data/opensource/
+unzip opensource.zip -d data/
 python main.py | tee output.txt
 """
 
